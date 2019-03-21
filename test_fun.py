d40b36b3ca848efdb6150234cd11efbea8572871
Leon <kenson2998@gmail.com>
2019-03-22 00:10:20 +0800

    update test1

--- test_fun.py
+++ test_fun.py
@@ -1,32 +1,37 @@
 def solution(ranks):
     listset = list(set(ranks))
     soldiers_father = []
+    soldiers_counts = {}
     counts = 0
     for i in listset:
         if i + 1 in ranks:
-            print(i, i + 1)
+            # print(i, i + 1)
             soldiers_father.append(i)
         for i in soldiers_father:
-            counts += 1
-
+            soldiers_counts[i] = ranks.count(i)
+    for key, v in soldiers_counts.items():
+        counts += v
+    return counts
 
 # solution([3, 4, 3, 0, 2, 2, 3, 0, 0])
 
 # ranks = [3, 4, 3, 0, 2, 2, 3, 0, 0] r5
-b =  [4, 4, 3, 3, 1, 0]
+b = [4, 4, 3, 3, 1, 0]
 a = list(set(b))
 c, d = {}, []
 e = 0
 for i in a:
     if i + 1 in b:
-        print(i, i + 1)
+        # print(i, i + 1)
         d.append(i)
-        print('d',d)
+        # print('d', d)
     for i in d:
         c[i] = b.count(i)
-        print('why',e)
-        e += 1
+        # print('why', e)
+        # e += 1
 f=0
 for key,v in c.items():
     f +=v
-print(c, e,f)
+print(f)
+
+print(solution([4, 4, 3, 3, 1, 0]))
